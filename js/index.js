1d5e7b1b77147171ac94c499c2b7cb8d86118c47
Rostyslav Holota <rostislavgolota03@gmail.com>
2024-10-05 20:29:31 +0200

    java-script homework

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const age = 16
+console.log(age);
+
+const name = "Rostyslav"
+console.log(name);
+
+const isStudent = true
+console.log(isStudent);
+
+const  myString = "Неважливо, як повільно ти рухаєшся, головне — не зупинятися. Кун Фу-цзи"
+console.log(myString);
+
+const b = 10
+const myNumber = a = b + 10;
+console.log(myNumber);
+
+const myNull = null
+console.log(myNull);
+
+const messege = prompt("Як тебе звати")
+console.log(messege);
+
+const data = confirm("Дозволити повідомлення?")
+if (data) {
+    alert ("Дякую за підтвердження!");
+}else{
+    alert ("Дію відмінено!");
+}
